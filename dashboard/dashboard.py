d76e8d5d4b20aaf8a70cf944f97e3f240f9cd04d
asterracea <alifwidya25@gmail.com>
2024-12-06 22:38:55 +0700

    fix error

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -45,7 +45,7 @@ hour_df.rename(columns={
 # mengubah dteday menjadi datetime
 hour_df["dateday"] = pd.to_datetime(hour_df["dateday"])
 
-monthly_rent_df = day_df.resample(rule='M', on='dateday').agg({
+monthly_rent_df = day_df.resample(rule='ME', on='dateday').agg({
     "casual": "sum",
     "registered": "sum",
     "count": "sum"
@@ -61,7 +61,7 @@ hour_df['weathersit'] = hour_df['weathersit'].map({
     3: 'Light Snow/Rain',
     4: 'Severe Weather'
 })
-monthly_rent_df = day_df.resample(rule='M', on='dateday').agg({
+monthly_rent_df = day_df.resample(rule='ME', on='dateday').agg({
     "casual": "sum",
     "registered": "sum",
     "count": "sum"
